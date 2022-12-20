e61eafe57b9cfcc3be69fe11cfa43eac9585c7da
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-12-19 17:33:59 -0800

    Campaign details page added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Sidebar } from './Sidebar';
 export { default as FormField } from './FormField';
 export { default as DisplayCampaigns } from './DisplayCampaigns';
 export { default as FundCard } from './FundCard';
+export { default as CountBox } from './CountBox';
