df69b0ef363decbc3fe09710fa9701f04abf7577
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-12-19 17:44:52 -0800

    Loader added to pages

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ethers } from 'ethers';
 
 import { useStateContext } from '../context';
-import { CustomButton, CountBox } from '../components';
+import { CustomButton, CountBox, Loader } from '../components';
 import { calculateBarPercentage, daysLeft } from '../utils';
 import { thirdweb } from '../assets';
 
 const CampaignDetails = () => {
+	const navigate = useNavigate();
 	const { state } = useLocation();
 	const { getDonations, donate, contract, address } = useStateContext();
 
@@ -35,12 +36,14 @@ const CampaignDetails = () => {
 
 		await donate(state.pId, amount);
 
+		navigate('/');
+
 		setIsLoading(false);
 	};
 
 	return (
 		<div>
-			{isLoading && 'Loading...'}
+			{isLoading && <Loader />}
 
 			<div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
 				<div className="flex-1 flex-col">
